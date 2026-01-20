4c23eb10f14b58eff374f233c951f980c67dea2c
P-Nastia <nastia.pyvovarchuck@gmail.com>
2026-01-20 14:51:05 +0200

    add cart models

---
+++ Backend/Core/Model/Cart/CartIngredientUnitModel.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace Core.Model.Cart;
+
+public class CartIngredientUnitModel
+{
+    public long UnitId { get; set; }
+    public string UnitName { get; set; } = string.Empty;
+    public decimal Amount { get; set; }
+}
